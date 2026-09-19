c494860ce022904b0422b4d7dfa466ed922fa761
Anonymous <grabik.pk@gmail.com>
2026-09-19 13:45:25 +0300

    Update nfqws-menu.sh

--- nfqws-menu.sh
+++ nfqws-menu.sh
@@ -10,7 +10,7 @@
 
 set -e
 
-SCRIPT_VERSION="0.6.46"
+SCRIPT_VERSION="0.6.43"
 
 REPO_URL="https://github.com/rndnaame/nfqws-menu"
 RAW_BASE="https://raw.githubusercontent.com/rndnaame/nfqws-menu/main"
